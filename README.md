c83caf685438c3dc7e3b8a972446885a6d52d176
minit <minit@mk1c.ont>
2021-01-14 15:05:57 -0600

    Add rsync.skip and --log support
    rsync.skip file causes a seq run directory to be skipped. First line of that file is logged as the reason
    --log LOGFILE provides concise logging of actions to that file. Add -v to make log more verbose

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Automatically rsync ONT sequencing data on a Mk1c or similar sequencing host to 
 
 This repo is expected to be cloned into MK1C:/data/user-scripts
 
-* rsync_reads.sh](rsync_reads.sh) SRCDIR DESTDIR [rsync_flags] 
+* [rsync_reads.sh](rsync_reads.sh) [--log LOGFILE [-v]] SRCDIR DESTDIR [rsync_flags] 
    * scans SRCDIR for sequencing runs, which are located by looking for "fast5_fail" directory. 
    * MD5 check sums are computed for all files, and a roll-up "md5sum" text file is created
    * The files are then rsync'ed to DESTDIR
@@ -12,11 +12,17 @@ This repo is expected to be cloned into MK1C:/data/user-scripts
    * if rsync succeeds, and "final_summary*.txt" exists, then "rsync.done" is created, and that sequencing directory will be skipped in the future
    * this script should be added to crontab using run-one (which may need to be installed ```apt install run-one```
       * run-one skips the next call, if the previous is still running (transfering data)
-      * crontab entry ```0,30 * * * * run-one /data/user_scripts/rsync_reads.sh /data USER@HOST:/TARGET_PATH```
+      * crontab entry 
+```
+# copy seq results to HPC cluster
+0,30 * * * * run-one /data/user_scripts/rsync_reads.sh --log /data/user_scripts/rsync_reads.log /data USER@HOST:/TARGET_PATH
+```
+   * if ```--log``` is given, logs concise messags to that file (one line per directory rsync'ed)
+      * -v increases the level of logging - adds SCAN, DONE, SKIP notifications
 
 
 
-* Makefile](Makefile)
+* [Makefile](Makefile)
    * target: "md5sum" 
       * computes .md5 files for sequencing and metadata files
       * creates roll-up "md5sum" 
--- rsync_reads.sh
+++ rsync_reads.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
+# set debug to "yes" for more logging
+VERBOSE=
+
+# 
+# parse args
+#
+if [[ "$1" == -*log ]]; then 
+    LOG=$2 
+    if [ ! -z "$LOG" ]; then echo $(date) ": $0 $*" >> $LOG; fi
+    shift 2
+fi
+if [[ "$1" == -*v* ]]; then 
+    VERBOSE=yes
+    shift 1
+fi
 SRCDIR=$1
 DESTDIR=$2
 shift 2
 RSYNC_ARGS="$*"
+
+if [[ -z "$SRCDIR" || -z "$DESTDIR" ]]; then 
+	echo "SYNTAX: $0 [-log logfile] SRC_DIR DEST_DIR [RSYNC_ARGS]"
+	exit 1
+fi
+
 #
 # constants
 #
 MAKEFILE="/data/user_scripts/Makefile"
+# make target and file name for combined run md5sums
 MD5SUM="md5sum"
 # use only 1 core, unless we know there is nothing running and can be greedier
 MD5SUM_CORES=1  
+# file to mark when a run is completely transfered and can be skipped in the future
 DONE="rsync.done"
-
-if [[ -z "$SRCDIR" || -z "$DESTDIR" ]]; then 
-	echo "SYNTAX: $0 SRC_DIR DEST_DIR [RSYNC_ARGS]"
-	exit 1
-fi
+# file to mark that a run should be skipped, for example if it will never be completed or is corrupt.
+SKIP="rsync.skip"
 
 #
 # find sequencing runs
@@ -26,6 +46,7 @@ SEQ_RUNS=$(find . -type d -name "fast5_fail" )
 for src in $SEQ_RUNS; do
 	srcd=$(dirname $src)
 	echo -e "\n\n##### checking $srcd ######"
+    	if [[ ! -z "$VERBOSE" &&  ! -z "$LOG" ]]; then echo " ---------" LOGGING SCAN to $LOG; echo SCAN $srcd >> $LOG; fi
 	FINAL_SUMMARY=$( echo $srcd/final_summary*.txt )
 
 	# 
@@ -33,10 +54,17 @@ for src in $SEQ_RUNS; do
 	#
 	if [[ -f $srcd/$DONE && -f $FINAL_SUMMARY ]]; then
 		if [[ $srcd/$DONE -nt $FINAL_SUMMARY ]]; then  
-			echo SKIP $srcd
+			if [[ ! -z "$VERBOSE" && ! -z "$LOG" ]]; then echo DONE $srcd >> $LOG; fi
+			echo COMPLETED $srcd
 			continue
 		fi
 	fi
+	if [[ -f $srcd/$SKIP ]]; then
+		if [[ ! -z "$VERBOSE" && ! -z "$LOG" ]]; then echo SKIP $srcd $(head -1 $srcd/$SKIP) >> $LOG; fi
+		echo SKIP $srcd
+		continue
+	fi
+	if [ ! -z "$LOG" ]; then echo SYNC $srcd >> $LOG; fi
 
 	# 
 	# generate/update md5 sums
@@ -61,6 +89,7 @@ for src in $SEQ_RUNS; do
 			echo "## exist: $MD5SUM $FINAL_SUMMARY"
 			if [[ $srcd/$MD5SUM -nt $FINAL_SUMMARY ]]; then
                         	echo "## COMPLETED $srcd"
+                        	if [ ! -z "$LOG" ]; then echo "MARK DONE $srcd" >> $LOG; fi
 				date > $srcd/$DONE
 			fi
 		else
